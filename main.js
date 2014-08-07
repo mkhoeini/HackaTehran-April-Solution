179394225228564cb274b6410359bfbe6fcd5b6d
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2014-08-07 13:14:59 +0430

    First iteration: add the needed skeleton for parsing html and generatin output

--- main.js
+++ main.js
@@ -1,53 +1,77 @@
-var PARAMS = process.argv.slice(2);
 
-var sentenceFile = PARAMS[0],
-    wordsFile = PARAMS[1],
-    inputFile = PARAMS[2],
-    outFile = PARAMS[3];
+var fs = require("fs"),
 
-var rw = require('rw');
+    pageFile = "page.html",
+    headlinesFile = "headlines.json",
 
-var sentences = JSON.parse(rw.readFileSync(sentenceFile, "utf8"));
-var words = JSON.parse(rw.readFileSync(wordsFile, "utf8"));
-var input = JSON.parse(rw.readFileSync(inputFile, "utf8"));
+    pageStr = fs.readFileSync(pageFile, "utf8"),
+    headlinesStr = fs.readFileSync(headlinesFile, "utf8"),
+    headlines = JSON.parse(headlinesStr),
 
-var result = [];
+    processedPage = processPage();
 
+fs.writeFileSync("result.html", processedPage, "utf8");
 
-input.forEach(function (numWords){
-  var sentSet = sentences[numWords];
-  var randomSentInd = Math.random() * sentSet.length |0;
-  var randSent = sentSet[randomSentInd];
-
-  var sentWords = randSent.split(" ");
-  var randWordInd = Math.random() * numWords |0;
-  var wordSet = [];
-
-  while (wordSet.length <= 1) {
-    wordSet = words[numWords++][randWordInd];
-  }
-
-  var res = replaceWord(sentWords, randWordInd, wordSet);
-
-  result.push(sentWords.join(" "));
-});
-
-function replaceWord(sentenceWords, index, wordSet)
+function processPage()
 {
-  var replaceWord = sentenceWords[index];
+  var splited = pageStr.split(/{{{|}}}/g);
 
-  while(replaceWord === sentenceWords[index])
+  for (var i = 1, len = splited.length; i < len; i+=2)
   {
-    randInd = Math.random() * Math.random() * wordSet.length |0;
-    replaceWord = wordSet[randInd];
+    var sentence = generateSentence(parseInt(splited[i]));
+    splited[i] = sentence;
   }
 
-  sentenceWords.splice(index, 1, replaceWord);
-  return sentenceWords.join(" ");
+  return splited.join(" ");
+}
+
+function generateSentence(len)
+{
+  return "{Placeholder}"
 }
 
 
-rw.writeFileSync(outFile, JSON.stringify(result), "utf8");
-
-
-
+// var sentences = JSON.parse(rw.readFileSync(sentenceFile, "utf8"));
+// var words = JSON.parse(rw.readFileSync(wordsFile, "utf8"));
+// var input = JSON.parse(rw.readFileSync(inputFile, "utf8"));
+//
+// var result = [];
+//
+//
+// input.forEach(function (numWords){
+//   var sentSet = sentences[numWords];
+//   var randomSentInd = Math.random() * sentSet.length |0;
+//   var randSent = sentSet[randomSentInd];
+//
+//   var sentWords = randSent.split(" ");
+//   var randWordInd = Math.random() * numWords |0;
+//   var wordSet = [];
+//
+//   while (wordSet.length <= 1) {
+//     wordSet = words[numWords++][randWordInd];
+//   }
+//
+//   var res = replaceWord(sentWords, randWordInd, wordSet);
+//
+//   result.push(sentWords.join(" "));
+// });
+//
+// function replaceWord(sentenceWords, index, wordSet)
+// {
+//   var replaceWord = sentenceWords[index];
+//
+//   while(replaceWord === sentenceWords[index])
+//   {
+//     randInd = Math.random() * Math.random() * wordSet.length |0;
+//     replaceWord = wordSet[randInd];
+//   }
+//
+//   sentenceWords.splice(index, 1, replaceWord);
+//   return sentenceWords.join(" ");
+// }
+//
+//
+// rw.writeFileSync(outFile, JSON.stringify(result), "utf8");
+//
+//
+//
